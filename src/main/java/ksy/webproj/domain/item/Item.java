bd5ea7133ffbb0f525b66ea779b98051c8c224ac
kimsiyong95 <kimsiyong95@gmail.com>
2022-06-08 21:53:41 +0900

    feat : 상품 도메인 비지니스 로직 추가

--- src/main/java/ksy/webproj/domain/item/Item.java
+++ src/main/java/ksy/webproj/domain/item/Item.java
@@ -1,6 +1,7 @@
 package ksy.webproj.domain.item;
 
 import ksy.webproj.domain.Category;
+import ksy.webproj.exception.NotEnoughStockException;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -25,4 +26,28 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+
+    // == 비지니스 로직 == //
+
+    /**
+     * stock 증가
+     * @param quantity
+     */
+    public void addStock(int quantity){
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock 감소
+     * @param quantity
+     */
+    public void removeStock(int quantity){
+        int restStock = this.stockQuantity - quantity;
+        if(restStock < 0){
+            throw  new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
+
 }
