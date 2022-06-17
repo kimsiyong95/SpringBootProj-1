91e56db6c0b5e22f5cfabb37f93a7b1d19d876b3
kimsiyong95 <kimsiyong95@gmail.com>
2022-06-17 15:55:27 +0900

    feat : 회원등록 개발

---
+++ src/main/java/ksy/webproj/controller/MemberController.java
@@ -0,0 +1,39 @@
+package ksy.webproj.controller;
+
+import ksy.webproj.domain.Address;
+import ksy.webproj.domain.Member;
+import ksy.webproj.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/members/new")
+    public String createForm(Model model){
+        model.addAttribute("memberForm", new MemberForm());
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(@Valid MemberForm memberForm){
+        Address address = new Address(memberForm.getCity(), memberForm.getStreet(), memberForm.getZipcode());
+
+        Member member = Member.builder()
+                              .name(memberForm.getName())
+                              .address(address)
+                              .build();
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+}
